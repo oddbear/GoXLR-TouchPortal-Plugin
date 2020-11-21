f4b12ab7d4c33e9a98d79da301226b6d8c811795
Oddbjørn U. Bakke <oddbjorn.bakke@gmail.com>
2020-11-21 17:31:56 +0100

    Models refactoring

---
+++ GoXLR.Models/Models/GetProfilesResponse.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+using GoXLR.Models.Models.Payloads;
+using GoXLR.Models.Models.Shared;
+
+namespace GoXLR.Models.Models
+{
+    //com.tchelicon.goXLR.ChangeProfile
+    //sendToPropertyInspector
+    public class GetProfilesResponse : ModelBase
+    {
+        [JsonPropertyName("payload")]
+        public GetProfilesResponsePayload Payload { get; set; }
+    }
+}
---
+++ GoXLR.Models/Models/Shared/RequestModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace GoXLR.Models.Models.Shared
+{
+    public class RequestModelBase : ModelBase
+    {
+        //The id of the controller device?
+        [JsonPropertyName("device")]
+        public string Device { get; set; }
+    }
+}
