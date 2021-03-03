04157798592819b9ea987a174aff683bf117a517
Oddbjørn Bakke <oddbjorn.bakke@gmail.com>
2021-03-03 17:43:32 +0100

    Changed from string to int in messages

--- Plugin/GoXLR.Plugin/Models/InfoMessage.cs
+++ Plugin/GoXLR.Plugin/Models/InfoMessage.cs
@@ -3,9 +3,9 @@
     public record InfoMessage(
         string Type,
         string TpVersionString,
-        string PluginVersion,
-        string TpVersionCode,
-        string SdkVersion,
+        int PluginVersion,
+        int TpVersionCode,
+        int SdkVersion,
         string Status
     ) : BaseMessage(Type);
 }
